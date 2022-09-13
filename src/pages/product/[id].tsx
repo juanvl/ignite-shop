67a769ded914e7edc99a5d547e48528041bab6fb
Juan Victor Leite <jnvleite@gmail.com>
2022-09-13 15:39:43 -0300

    style: add theme colors and fonts

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -3,9 +3,5 @@ import { useRouter } from 'next/router'
 export default function ProductDetail() {
   const { query } = useRouter()
 
-  return (
-    <div className="text-3xl font-bold underline">
-      Product id: {JSON.stringify(query)}
-    </div>
-  )
+  return <div>Product id: {JSON.stringify(query)}</div>
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        white: '#ffffff',
+
+        gray900: '#121214',
+        gray800: '#202024',
+        gray300: '#c4c4cc',
+        gray100: '#e1e1e6',
+
+        green500: '#00875f',
+        green300: '#00b37e',
+      },
+    },
   },
   plugins: [require('@tailwindcss/forms'), require('tailwind-scrollbar')],
 }
