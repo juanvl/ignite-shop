8528d15da09411c7b5dbb20ebffde01cf74fe644
Juan Victor Leite <jnvleite@gmail.com>
2022-09-13 16:21:29 -0300

    feat: add header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
-import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 
+import { AppProps } from 'next/app'
+import Image from 'next/image'
+
+import logo from '../assets/logo.svg'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="flex min-h-screen flex-col items-start justify-center ">
+      <header className="my-0 mx-auto w-full max-w-[1180] py-8">
+        <Image src={logo} alt="Ignite shop logo" />
+      </header>
+
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
